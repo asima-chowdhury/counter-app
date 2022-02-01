3237e622c6de22358accfc3d25b3b76a73a1750b
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-02-01 12:46:21 +0600

    setting attribute

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,19 +5,31 @@ class counter extends Component {
         count: 0
     }
 
+    styles = {
+        fontSize: 20,
+        fontWeight: 'bold',
+    };
+
     render() {
         return (
             <div>
                 {/* <span>{this.state.count}</span> */}
-                <span>{this.formatCount()}</span> 
-                <button>increment</button>
+                {/* <span style={this.styles} className="badge badge-primary m-2">
+                    {this.formatCount()}
+                </span>  */}
+
+                <span style={{ fontSize: 10 }} className="badge badge-primary m-2">{this.formatCount()}</span>
+                <button className="btn btn-secondary btn-sm">increment</button>
             </div>
         );
     }
 
-    formatCount(){
-        // const count = this.state;
-        return this.state.count === 0 ? 'Zero' : this.state.count;
+    formatCount() {
+
+        // return this.state.count === 0 ? 'Zero' : this.state.count;
+        const { count } = this.state;
+        // return count === 0 ? <h2>Zero</h2> : count;        
+        return count === 0 ? 'Zero' : count;
 
     }
 }
