156b9ddec65ca21d6585b70d55071ad994e82f74
Asima Chowdhury <67515830+asima-chowdhury@users.noreply.github.com>
2022-02-08 07:50:50 +0600

    sent {counter} as a props from counters parent to child counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,21 @@ import React, { Component } from 'react';
 
 class counter extends Component {
     state = {
-        value: this.props.value,
+        value: this.props.counter.value,
     };
 
     handleIncrement = () => {
-        this.setState({ count: this.state.value + 1 })
+        this.setState({ value: this.state.value + 1 })
     }
 
     render() {
-        console.log('props', this.props);
+        // console.log('props', this.props);
         return (
             <div>
                 {this.props.children}
                 <span style={{ fontSize: 10 }} className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm my-1">increment</button>
-                <button onClick={()=>this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-1">Delete</button>
+                <button onClick={()=>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-1">Delete</button>
             </div>
         );
     }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -21,7 +21,7 @@ class Counters extends Component {
         return (
             <div>
                 {this.state.counters.map(counter => (
-                    <Counter key={counter.id} value={counter.value} onDelete={this.handleDelete} id={counter.id} />
+                    <Counter key={counter.id} onDelete={this.handleDelete} counter={counter} />
                 ))}
             </div>
         );
